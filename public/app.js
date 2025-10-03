13fe68eec133d02fb838e677dd63328cea5dfb18
erica-r05 <reginaerica073@gmail.com>
2025-10-03 15:00:41 -0300

    Detalhes Dinamica

--- public/app.js
+++ public/app.js
@@ -9,7 +9,7 @@ const portfolioItems = {
     },
     {
         id: 2,
-        title: "A Beleza de Reina",
+        titulo:"A Beleza de Reina",
         artista: "Erica Regina",
         descricao: "Uma representação da serenidade.",
         imagem: "images/reina.jpg"
@@ -33,7 +33,7 @@ const portfolioItems = {
         titulo: "O Eco da Dor",
         artista: "Erica Regina",
         descricao: "A dor representada é o reflexo (eco) de algo além do que está sendo mostrado.",
-        imagem:"images/o_eco_da_dor.jpg"
+        imagem:"images/eco_da_dor.jpg"
     }
     ],
     albunsFavoritos: [
@@ -41,6 +41,7 @@ const portfolioItems = {
             id: 6,
             titulo: "Hard to imagine the neighbourhood ever changing",
             artista: "The Neighbourhood",
+            descricao: "Hard to Imagine the Neighbourhood Ever Changing é o segundo álbum de estúdio da banda americana The Neighbourhood, lançado em 2018. O álbum apresenta uma mistura de rock alternativo, indie pop e R&B, com letras introspectivas e sombrias que exploram temas como amor, perda e identidade. Com faixas como 'Scary Love' e 'Stargazing', o álbum recebeu críticas positivas por sua produção atmosférica e coesão temática, consolidando a posição da banda na cena musical contemporânea.",
             imagem: "images/hard_to_imagine_the_neighbourhood_ever_changing.jpg",
             categoria: "albuns"
         },
@@ -48,6 +49,7 @@ const portfolioItems = {
             id: 7,
             titulo: "Nevermind",
             artista: "Nirvana",
+            descricao: "Nevermind é o segundo álbum de estúdio da banda americana Nirvana, lançado em 1991. Produzido por Butch Vig, o álbum é amplamente creditado por trazer o grunge e o rock alternativo para o mainstream. Com faixas icônicas como 'Smells Like Teen Spirit', 'Come as You Are' e 'Lithium', Nevermind apresenta uma mistura de riffs pesados, melodias cativantes e letras introspectivas que capturam a angústia e a alienação da juventude dos anos 90. O álbum foi um sucesso comercial e crítico, vendendo mais de 30 milhões de cópias em todo o mundo e solidificando o legado do Nirvana como uma das bandas mais influentes da história do rock.",
             imagem: "images/nevermind - Nirvana.jpg",
             categoria: "albuns", 
         },
@@ -55,6 +57,7 @@ const portfolioItems = {
             id: 8,
             titulo: "Freudian",
             artista: "Daniel Cesar",
+            descricao: "Freudian é o álbum de estreia do cantor e compositor canadense Daniel Caesar, lançado em 2017. O álbum apresenta uma mistura de R&B contemporâneo, soul e gospel, com letras introspectivas que exploram temas como amor, fé e identidade. Com faixas como 'Get You', 'Best Part' e 'We Find Love', Freudian é elogiado por sua produção suave, vocais emotivos e arranjos minimalistas que destacam a vulnerabilidade e a sinceridade de Caesar. O álbum recebeu aclamação crítica e foi indicado ao Grammy, solidificando Daniel Caesar como uma das vozes emergentes mais promissoras do R&B moderno.",
             imagem: "images/freudian.jpg",
             categoria: "albuns"
         },
@@ -62,6 +65,7 @@ const portfolioItems = {
             id: 9,
             titulo: "Djavan ao vivo 1999",
             artista: "Djavan",
+            descricao: "Djavan ao Vivo 1999 é um álbum ao vivo do renomado cantor e compositor brasileiro Djavan, lançado em 1999. O álbum captura a energia e a emoção das performances ao vivo de Djavan, apresentando uma seleção de suas músicas mais populares e queridas, incluindo sucessos como 'Oceano', 'Flor de Lis' e 'Sina'. Com arranjos ao vivo vibrantes e a habilidade vocal distinta de Djavan, o álbum oferece uma experiência autêntica e envolvente para os fãs, destacando sua versatilidade musical que abrange gêneros como MPB, jazz e pop. Djavan ao Vivo 1999 é uma celebração do talento duradouro de Djavan e sua contribuição significativa para a música brasileira.",
             imagem: "images/djavan_ao_vivo_1999.jpg",
             categoria: "albuns",
         },
@@ -69,6 +73,7 @@ const portfolioItems = {
             id: 10,
             titulo: "Dam",
             artista: "Kendrick Lamar",
+            descricao: "Damn é o quarto álbum de estúdio do rapper americano Kendrick Lamar, lançado em 2017. O álbum é uma exploração profunda de temas como identidade, fé, luta e redenção, apresentando uma mistura de estilos musicais que vão do hip-hop ao jazz e ao funk. Com faixas impactantes como 'HUMBLE.', 'DNA.' e 'LOVE.', Damn é elogiado por suas letras complexas, produção inovadora e a habilidade de Lamar em contar histórias poderosas. O álbum recebeu aclamação crítica generalizada, ganhou o Prêmio Pulitzer de Música em 2018 e solidificou Kendrick Lamar como um dos artistas mais influentes e respeitados da música contemporânea.",   
             imagem: "images/damn_KendrickLamar.jpg",
             categoria: "albuns",
         }
@@ -113,6 +118,7 @@ const portfolioItems = {
             descricao:"A obra Noite Estrelada sobre o Ródano, de Vincent van Gogh, é uma pintura de 1888 que retrata a vista do rio Ródano em Arles, França, sob um céu noturno estrelado. Com suas cores vibrantes e pinceladas expressivas, Van Gogh capturou a beleza e a tranquilidade da cena, destacando o reflexo das estrelas na água. A obra é um exemplo marcante do estilo pós-impressionista de Van Gogh, transmitindo emoção e movimento através da cor e da luz.",
             imagem: "images/Starry_night_over_the_Rhone.jpg",
             categoria: "pinturas",
+
         }
     ],
 
@@ -123,7 +129,7 @@ function criarCard(item) {
             <img src="${item.imagem}" class="card-img-top" alt="${item.titulo}">
             <div class="item-info">
                 <h3 class="card-title">${item.titulo}</h3>
-                <p class="card-text">${item.descricao}</p>
+                <p class="card-text">${item.descricao || ''}</p>
                 <p class="card-artista">${item.artista}</p>
                 <a href="detalhes.html?id=${item.id}" class="btn-detalhes">Ver Detalhes</a>
             </div>
@@ -142,7 +148,7 @@ function adicionarIdsAosLinks() {
         'Apenas um sketch comum': 4,
         'O eco da dor': 5,
         
-        'Hard to imagine th neighbourhood ever changing': 6,
+        'Hard to imagine the neighbourhood ever changing': 6,
         'Nevermind': 7,
         'Freudian': 8,
         'Djavan ao vivo 1999': 9,
